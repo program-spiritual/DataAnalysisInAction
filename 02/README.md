6daf02bfc26dd804bfad996d24e357b34b205ec8
huhongyun <xiaomiwujiecao@gmail.com>
2018-12-19 16:57:05 +0800

    极客时间 数据分析实战45讲详细笔记 初始化

---
+++ 02/README.md
@@ -0,0 +1,11 @@
+## 02 学习数据挖掘的最佳路径是什么 笔记
+
+### 笔记
+
+![最佳路径](./bestpath.png)
+
+## 思维导图
+
+![思维导图](./bestPathMind.png)
+
+## FreeMind 文件 见目录 `02/bestPath.mm`
---
+++ 02/bestPath.mm
@@ -0,0 +1,99 @@
+<map>
+		<node ID="root" TEXT="数据挖掘">		<node TEXT="数据挖掘的基本流程" ID="17c167c55026bf062" STYLE="bubble" POSITION="right">
+		<node TEXT="商业理解" ID="6d167c550b8db0a8" STYLE="fork">
+		</node>
+		<node TEXT="数据理解" ID="3a0167c550f2c1065" STYLE="fork">
+		</node>
+		<node TEXT="数据准备" ID="4b167c551033303" STYLE="fork">
+		</node>
+		<node TEXT="模型建立" ID="13d167c5511f9b074" STYLE="fork">
+		</node>
+		<node TEXT="模型评估" ID="17f167c5512d7b0ce" STYLE="fork">
+		</node>
+		<node TEXT="上线发布" ID="392167c5513bd907d" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="数据挖掘的十大算法" ID="13167c56292cb0fe" STYLE="bubble" POSITION="right">
+		<node TEXT="分类算法" ID="207167c5665d12189" STYLE="fork">
+		<node TEXT="C4.5" ID="79167c56346f0138" STYLE="fork">
+		</node>
+		<node TEXT="朴素贝叶斯" ID="8b167c56491b1091" STYLE="fork">
+		</node>
+		<node TEXT="SVM" ID="1eb167c569361c164" STYLE="fork">
+		</node>
+		<node TEXT="KNN" ID="3a1167c56a070f0d8" STYLE="fork">
+		</node>
+		<node TEXT="AdaBoost" ID="2cf167c56b1e200b5" STYLE="fork">
+		</node>
+		<node TEXT="CART" ID="6f167c56cd91108a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="聚类算法" ID="153167c56d33640bb" STYLE="fork">
+		<node TEXT="K-Means" ID="11c167c56e833318a" STYLE="fork">
+		</node>
+		<node TEXT="EM" ID="3c0167c573b111049" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="关联分析" ID="2d9167c5755e950f" STYLE="fork">
+		<node TEXT="Apriori" ID="245167c576393508a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="连接分析" ID="35f167c575ab01128" STYLE="fork">
+		<node TEXT="PageRank" ID="17d167c577e73c071" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="数据挖掘的数学原理" ID="72167c57c5b400ff" STYLE="bubble" POSITION="left">
+		<node TEXT="1. 概率和数学统计" ID="164167c57c89360df" STYLE="fork">
+		<node TEXT="条件概率" ID="1f4167c57d86150dd" STYLE="fork">
+		</node>
+		<node TEXT="独立性" ID="31167c57e088c04e" STYLE="fork">
+		</node>
+		<node TEXT="随机变量" ID="1ed167c57e18cb0c3" STYLE="fork">
+		</node>
+		<node TEXT="多维随机变量" ID="140167c57e2b650bd" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="2.线性代数" ID="3bc167c57ccf0d05d" STYLE="fork">
+		<node TEXT="特征值" ID="1b7167c57e64bf0ad" STYLE="fork">
+		</node>
+		<node TEXT="特征向量" ID="2f6167c57ec904086" STYLE="fork">
+		</node>
+		<node TEXT="用特征向量近似代表物体的特征" ID="2e5167c57edf7305" STYLE="fork">
+		</node>
+		<node TEXT="矩阵" ID="355167c57fe5a909" STYLE="fork">
+		<node TEXT="PCA方法" ID="e7167c57ffc23002" STYLE="fork">
+		</node>
+		<node TEXT="SVD方法" ID="140167c58031ae0db" STYLE="fork">
+		</node>
+		<node TEXT="MF方法" ID="28167c5805ac2045" STYLE="fork">
+		</node>
+		<node TEXT="NMF方法" ID="28b167c5807b2f17e" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="3. 图论" ID="82167c57cf33d0c7" STYLE="fork">
+		<node TEXT="起源自社交网络的好友关系表达" ID="3d1167c58102290dc" STYLE="fork">
+		</node>
+		<node TEXT="人与人之间的关系可以用图论上的两个节点记性连接" ID="13d167c58128a0154" STYLE="fork">
+		</node>
+		<node TEXT="节点的度可以理解为一个人的朋友数" ID="22d167c587b1530e3" STYLE="fork">
+		</node>
+		<node TEXT="图论对网络结构的分析非常有效" ID="3b1167c58824e20e6" STYLE="fork">
+		</node>
+		<node TEXT="图论在关系挖掘、图像分割中有重要的作用" ID="5c167c588a3da01c" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="4. 最优化方法" ID="61167c57d58e80ab" STYLE="fork">
+		<node TEXT="相当于机器学习中的自我学习过程" ID="2ce167c5891e0d17c" STYLE="fork">
+		</node>
+		<node TEXT="训练结果出现差异就要迭代调整" ID="2d3167c58994520b6" STYLE="fork">
+		</node>
+		<node TEXT="一般的 这个过程是漫长 随机的" ID="d2167c589eaab089" STYLE="fork">
+		</node>
+		<node TEXT="这个算法的退出就是为了用更短的时间得到收敛" ID="216167c58a2fad133" STYLE="fork">
+		</node>
+		</node>
+		</node>
+</node>
+</map>
