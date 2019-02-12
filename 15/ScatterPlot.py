823c80f9086d5c41d714e0cb4659b9bc378575d4
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-12 09:06:13 +0800

    add chapter 15

---
+++ 15/ScatterPlot.py
@@ -0,0 +1,10 @@
+# encoding:utf-8
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+## 散点图
+
+plt.scatter(200,200,marker='x')
+plt.show()
+
---
+++ 15/useseaborn.py
@@ -0,0 +1,25 @@
+# encoding:utf-8
+import matplotlib
+
+matplotlib.use('TkAgg')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# import seaborn as sns
+
+# sns.set(style="darkgrid")
+
+N = 1000
+x = np.random.randn(N)
+y = np.random.rand(N)
+
+## use matplotlib
+plt.scatter(x, y, marker='x')
+plt.show()
+
+## use seaborn
+
+# df = pd.DataFrame({'x': x, 'y': y})
+#
+# sns.jointplot(x='x', y='y', data=df, kind='scatter')
+# plt.show()
