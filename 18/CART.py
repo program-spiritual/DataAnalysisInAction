f44a09ed8021e7e7ed28629f0952f4d1f856e984
huhongyun <xiaomiwujiecao@gmail.com>
2019-02-28 11:44:23 +0800

    更新pipenv的环境为3.6.7 更新镜像源为清华大学镜像

---
+++ 18/CART.py
@@ -0,0 +1,25 @@
+# encoding=utf-8
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import load_iris
+# 准备数据集
+iris=load_iris()
+# 获取特征集和分类标识
+features = iris.data
+labels = iris.target
+# 随机抽取 33% 的数据作为测试集，其余为训练集
+train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.33, random_state=0)
+# 创建 CART 分类树
+clf = DecisionTreeClassifier(criterion='gini')
+# 拟合构造 CART 分类树
+clf = clf.fit(train_features, train_labels)
+# 用 CART 分类树做预测
+test_predict = clf.predict(test_features)
+# 预测结果与测试集结果作比对
+score = accuracy_score(test_labels, test_predict)
+print("CART 分类树准确率 %.4lf" % score)
+
+'''output
+CART 分类树准确率 0.9800
+'''
