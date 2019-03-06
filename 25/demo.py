b7f434f9e83ef4f43903dd54c14ed3abf7db352b
huhongyun <xiaomiwujiecao@gmail.com>
2019-03-06 15:13:14 +0800

    更新至25讲

---
+++ 25/demo.py
@@ -0,0 +1,16 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+
+# 加载数据
+digits = load_digits()
+data = digits.data
+# 数据探索
+print(data.shape)
+# 查看第一幅图像
+print(digits.images[0])
+# 第一幅图像代表的数字含义
+print(digits.target[0])
+# 将第一幅图像显示出来
+plt.gray()
+plt.imshow(digits.images[0])
+plt.show()
